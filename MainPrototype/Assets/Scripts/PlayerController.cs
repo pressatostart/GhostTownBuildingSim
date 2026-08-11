503c43a3f764ddf5f99d03fae58f0b5a782d8887
Naomi Burgess <naomi.samantha.burgess@gmail.com>
2026-08-11 13:27:10 -0400

    Added simple walk animations.

--- MainPrototype/Assets/Scripts/PlayerController.cs
+++ MainPrototype/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,37 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 2;
 
-    private Vector2 moveDir;
+    [SerializeField]
+    private Animator _anim;
+
+    private Vector2 _moveDir;
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveDir = context.ReadValue<Vector2>();
+        _moveDir = context.ReadValue<Vector2>();
+
+        if (_anim != null )
+        {
+            if (_moveDir.magnitude > 0)
+            {
+                _anim.SetBool("isMoving", true);
+                _anim.SetFloat("horizontal", _moveDir.x);
+                _anim.SetFloat("vertical", _moveDir.y);
+            }
+            else
+            {
+                _anim.SetBool("isMoving", false);
+            }
+        }
     }
 
     public void Update()
     {
-        transform.position += new Vector3(moveDir.x, moveDir.y) * _speed * Time.deltaTime;
+        transform.position += new Vector3(_moveDir.x, _moveDir.y) * _speed * Time.deltaTime;
     }
 }
